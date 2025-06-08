801eaddfa6ac20e58bad281ebea41f57245d2bf7
Abdelrahman Magdy <magdyabdelrhman868@gmail.com>
2025-06-08 21:16:34 +0300

    Verify signup page for redirect issues

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-
+import React from 'react'
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import CategoryPreview from '../components/CategoryPreview';
@@ -8,9 +8,9 @@ import WinterSection from '../components/WinterSection';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 
-const Index = () => {
+const Index: React.FC = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background">
       <Header />
       <HeroSection />
       <CategoryPreview />
